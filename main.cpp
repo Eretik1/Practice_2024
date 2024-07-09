06c10d88439cdf0ce9a3fcb3424f33eab311cf32
Ivan <ivanbolbin@gmail.com>
2024-07-09 21:32:09 +0700

    Fourth commit. Доработано главное меню, добавлен текст, подключены шрифты.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include <SFML/Graphics.hpp>
+#include <SFML/OpenGL.hpp>
 
 
 std::vector<std::string> readFile() {
@@ -95,25 +96,24 @@ int main1()
     return 0;
 }
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Buttons Example", sf::Style::Titlebar | sf::Style::Close);
+void mainMenu(){
+    sf::RenderWindow window(sf::VideoMode(400, 600), "Сards", sf::Style::Default);
+    
+    sf::RectangleShape button1(sf::Vector2f(250, 50));
+    button1.setPosition(75, 50);
+    button1.setFillColor(sf::Color::Black);
 
-    // Create buttons
-    sf::RectangleShape button1(sf::Vector2f(200, 50));
-    button1.setPosition(50, 50);
-    button1.setFillColor(sf::Color::Green);
+    sf::RectangleShape button2(sf::Vector2f(250, 50));
+    button2.setPosition(75, 150);
+    button2.setFillColor(sf::Color::Black);
 
-    sf::RectangleShape button2(sf::Vector2f(200, 50));
-    button2.setPosition(50, 150);
-    button2.setFillColor(sf::Color::Blue);
+    sf::RectangleShape button3(sf::Vector2f(250, 50));
+    button3.setPosition(75, 250);
+    button3.setFillColor(sf::Color::Black);
 
-    sf::RectangleShape button3(sf::Vector2f(200, 50));
-    button3.setPosition(50, 250);
-    button3.setFillColor(sf::Color::Red);
-
-    sf::RectangleShape exitButton(sf::Vector2f(200, 50));
-    exitButton.setPosition(50, 350);
-    exitButton.setFillColor(sf::Color::White);
+    sf::RectangleShape exitButton(sf::Vector2f(100, 50));
+    exitButton.setPosition(150, 500);
+    exitButton.setFillColor(sf::Color::Red);
 
     while (window.isOpen()) {
         sf::Event event;
@@ -138,14 +138,103 @@ int main() {
                 }
             }
         }
-
-        window.clear();
+        window.clear(sf::Color::White);
         window.draw(button1);
         window.draw(button2);
         window.draw(button3);
         window.draw(exitButton);
         window.display();
     }
+}
+
+void mainMenu2(){
+    sf::Font font;
+    font.loadFromFile("D:\\projects\\practice2024\\arial.ttf");
+    
+    sf::RenderWindow window(sf::VideoMode(400, 600), "Сards", sf::Style::Default);
+    
+    sf::RectangleShape button1(sf::Vector2f(250, 50));
+    button1.setPosition(75, 50);
+    button1.setFillColor(sf::Color::Black);
+
+    sf::Text text1;
+    text1.setFont(font); // Установите шрифт для текста
+    text1.setString("Button 1"); // Установите текст
+    text1.setCharacterSize(20); // Установите размер текста
+    text1.setFillColor(sf::Color::White); // Установите цвет текста
+    text1.setPosition(95, 60); // Установите позицию текста
+
+    sf::RectangleShape button2(sf::Vector2f(250, 50));
+    button2.setPosition(75, 150);
+    button2.setFillColor(sf::Color::Black);
+
+    sf::Text text2;
+    text2.setFont(font);
+    text2.setString("Button 2");
+    text2.setCharacterSize(20);
+    text2.setFillColor(sf::Color::White);
+    text2.setPosition(95, 160);
+
+    sf::RectangleShape button3(sf::Vector2f(250, 50));
+    button3.setPosition(75, 250);
+    button3.setFillColor(sf::Color::Black);
+
+    sf::Text text3;
+    text3.setFont(font);
+    text3.setString("Button 3");
+    text3.setCharacterSize(20);
+    text3.setFillColor(sf::Color::White);
+    text3.setPosition(95, 260);
+
+    sf::RectangleShape exitButton(sf::Vector2f(100, 50));
+    exitButton.setPosition(150, 500);
+    exitButton.setFillColor(sf::Color::Red);
+
+    sf::Text exitText;
+    exitText.setFont(font);
+    exitText.setString("Exit");
+    exitText.setCharacterSize(20);
+    exitText.setFillColor(sf::Color::White);
+    exitText.setPosition(180, 510);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+            if (event.type == sf::Event::MouseButtonPressed) {
+                sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+
+                if (button1.getGlobalBounds().contains(mousePos.x, mousePos.y)) {
+                    // Button 1 pressed
+                    // Add your code here
+                } else if (button2.getGlobalBounds().contains(mousePos.x, mousePos.y)) {
+                    // Button 2 pressed
+                    // Add your code here
+                } else if (button3.getGlobalBounds().contains(mousePos.x, mousePos.y)) {
+                    // Button 3 pressed
+                    // Add your code here
+                } else if (exitButton.getGlobalBounds().contains(mousePos.x, mousePos.y)) {
+                    window.close();
+                }
+            }
+        }
+        window.clear(sf::Color::White);
+        window.draw(button1);
+        window.draw(text1);
+        window.draw(button2);
+        window.draw(text2);
+        window.draw(button3);
+        window.draw(text3);
+        window.draw(exitButton);
+        window.draw(exitText);
+        window.display();
+    }
+}
+
+int main() {
+    mainMenu2();
 
     return 0;
 }
